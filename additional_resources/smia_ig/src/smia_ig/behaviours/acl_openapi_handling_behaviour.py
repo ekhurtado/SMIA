1bca34d8fd424673d0706f0bbbc7c3bb133006c3
ekhurtado <e16hurtado@gmail.com>
2025-05-26 11:56:23 +0100

    test: testing GetAssetIDsOfCapability with SMIA KB and valid capability IRI

--- additional_resources/smia_ig/src/smia_ig/behaviours/acl_openapi_handling_behaviour.py
+++ additional_resources/smia_ig/src/smia_ig/behaviours/acl_openapi_handling_behaviour.py
@@ -48,10 +48,10 @@ class ACLOpenAPIHandlingBehaviour(CyclicBehaviour):
 
         # TODO TEST
         try:
-            params = {'asset_id': 'myasset001'}
-            result = await self.myagent.acl_openapi_services.execute_agent_service_by_id('GetSMIAInstanceByAssetId',
-                                                                                         **params)
-            params = {'capability_iri': 'myasset001'}
+            # params = {'asset_id': 'myasset001'}
+            # result = await self.myagent.acl_openapi_services.execute_agent_service_by_id('GetSMIAInstanceByAssetId',
+            #                                                                              **params)
+            params = {'capability_iri': 'http://www.w3id.org/upv-ehu/gcis/css-smia#Negotiation'}
             result = await self.myagent.acl_openapi_services.execute_agent_service_by_id('GetAssetIDsOfCapability',
                                                                                          **params)
         except ValueError as e:
