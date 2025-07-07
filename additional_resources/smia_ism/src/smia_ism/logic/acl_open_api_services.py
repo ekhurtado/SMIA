a7d6438df7d25e49db0afe5bf9e831d619ba32ed
ekhurtado <e16hurtado@gmail.com>
2025-07-07 12:02:43 +0100

    feat: added AAS Repository Infrastructure management. Started with the service to obtain an AAS by its identifier

--- additional_resources/smia_ism/src/smia_ism/logic/acl_open_api_services.py
+++ additional_resources/smia_ism/src/smia_ism/logic/acl_open_api_services.py
@@ -2,6 +2,7 @@ import logging
 
 from smia.utilities.aas_related_services_info import AASRelatedServicesInfo
 
+from external_infrastructures.aas_repository_infrastructure import AASRepositoryInfrastructure
 from external_infrastructures.smia_kb_infrastructure import SMIAKBInfrastructure
 from utilities.acl_open_api_utils import send_openapi_http_get_request, send_openapi_http_post_request, \
     check_and_get_response_error
@@ -34,6 +35,7 @@ class ACLOpenAPIServices:
     # ---------------------------------
     # Infrastructure discovery services
     # ---------------------------------
+    # SMIA KB services --->
     @staticmethod
     def get_smia_instance_by_asset_id(asset_id):
         if asset_id is None:
@@ -73,6 +75,34 @@ class ACLOpenAPIServices:
         assets_ids = [asset_data['id'] for asset_data in assets_json]
         return assets_ids
 
+    # AAS Repository services --->
+    @staticmethod
+    def get_asset_adminitration_shell_by_id(aas_id):
+        """
+        This method gets the administration shell from the AAS Repository by its identifier.
+
+        Args:
+            aas_id (str): The AAS identifier.
+
+        Returns:
+            object: The AAS object from the AAS Repository.
+        """
+        if aas_id is None:
+            return "ERROR: The AAS identifier cannot be null."
+        # First, the AAS JSON object need to be obtained
+        aas_json = send_openapi_http_get_request(AASRepositoryInfrastructure.get_aas_json_url_by_id(aas_id))
+        if 'ERROR' in check_and_get_response_error(aas_json):
+            return 'Cannot obtain the AAS. {}'.format(check_and_get_response_error(aas_json))
+        aas_complete_json = {'assetAdministrationShells': [aas_json], 'submodels': []}
+        # Then, each Submodel JSON related to the AAS need to bje obtained
+        for submodel_ref_data in aas_json['submodels']:
+            submodel_json = send_openapi_http_get_request(AASRepositoryInfrastructure.get_submodel_json_url_by_id(
+                submodel_ref_data['keys'][0]['value']))
+            aas_complete_json['submodels'].append(submodel_json)
+        # Finally, the data will be cleaned to comply with the latest version of the AAS meta-model so that BaSyx SDK
+        # does not present errors
+        return AASRepositoryInfrastructure.clean_aas_json_information(aas_complete_json)
+
 
 
     ACLOpenAPIServicesMap = {
@@ -82,4 +112,6 @@ class ACLOpenAPIServices:
         AASRelatedServicesInfo.AAS_INFRASTRUCTURE_DISCOVERY_SERVICE_GET_SMIA_BY_ASSET: get_smia_instance_by_asset_id,
         AASRelatedServicesInfo.AAS_INFRASTRUCTURE_DISCOVERY_SERVICE_GET_ASSET_BY_SMIA: get_asset_id_by_smia_instance,
         AASRelatedServicesInfo.AAS_INFRASTRUCTURE_DISCOVERY_SERVICE_GET_ALL_ASSET_BY_CAPABILITY: get_assets_ids_of_capability,
+
+        'GetAssetAdministrationShellById': get_asset_adminitration_shell_by_id,    # TODO AÑADIRLO EN SMIA
     }  #: This object maps the service identifiers with its associated execution methods
