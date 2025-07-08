5553116dc6da76e57e834cd731dd6172723e6325
ekhurtado <e16hurtado@gmail.com>
2025-07-08 13:55:45 +0100

    fix: fixed error saving timestamps for analysis of SMIA performance in CeDRI tests

--- src/smia/behaviours/specific_handle_behaviours/handle_capability_behaviour.py
+++ src/smia/behaviours/specific_handle_behaviours/handle_capability_behaviour.py
@@ -426,26 +426,22 @@ class HandleCapabilityBehaviour(OneShotBehaviour):
 
             # TODO BORRAR: PARA PRUEBAS CEDRI
             if 'smia-pe' in self.received_acl_msg.thread:
+                # TODO BORRAR -> es para obtener los datos para el analisis
+                from smia.utilities import smia_archive_utils, smia_general_info
+                import asyncio
+                await smia_archive_utils.save_csv_metrics_timestamp(
+                    smia_general_info.SMIAGeneralInfo.CONFIGURATION_AAS_FOLDER_PATH + '/metrics', self.myagent.jid,
+                    f"Asset service requested [{self.received_acl_msg.thread}]")
+                await asyncio.sleep(1.5)
+                # TODO BORRAR -> es para obtener los datos para el analisis
+                await smia_archive_utils.save_csv_metrics_timestamp(
+                    smia_general_info.SMIAGeneralInfo.CONFIGURATION_AAS_FOLDER_PATH + '/metrics', self.myagent.jid,
+                    f"Asset service completed [{self.received_acl_msg.thread}]")
                 return {'status': 'success', 'prueba': 'cedri'}
-
-            # TODO BORRAR -> es para obtener los datos para el analisis
-            from smia.utilities import smia_archive_utils, smia_general_info
-            await smia_archive_utils.save_csv_metrics_timestamp(
-                smia_general_info.SMIAGeneralInfo.CONFIGURATION_AAS_FOLDER_PATH + '/metrics', self.myagent.jid,
-                f"Asset service requested [{self.received_acl_msg.thread}]")
-            await smia_archive_utils.save_csv_metrics_timestamp(
-                smia_general_info.SMIAGeneralInfo.CONFIGURATION_AAS_FOLDER_PATH + '/metrics', self.myagent.jid,
-                "Asset service requested [{}]".format(self.received_acl_msg.thread))
-            _logger.assetinfo("Saved timestamp!!!.")
 
             skill_execution_result = await asset_connection_class.execute_asset_service(
                 interaction_metadata=aas_skill_interface_elem, service_input_data=received_skill_input_data)
             _logger.assetinfo("Skill of the capability successfully executed.")
-
-            # TODO BORRAR -> es para obtener los datos para el analisis
-            await smia_archive_utils.save_csv_metrics_timestamp(
-                smia_general_info.SMIAGeneralInfo.CONFIGURATION_AAS_FOLDER_PATH + '/metrics', self.myagent.jid,
-                f"Asset service completed [{self.received_acl_msg.thread}]")
 
             # TODO SI LA SKILL TIENE OUTPUT PARAMETERS, HAY QUE RECOGERLOS DEL skill_execution_result. En ese caso, se
             #  sobreescribirá el skill_execution_result con la variable output y su valor (el cual será lo que devolverá el
