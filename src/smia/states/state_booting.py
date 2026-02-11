4e5834cbfc913981ebced60a0e7f42c3d0fb7cdf
ekhurtado <e16hurtado@gmail.com>
2026-02-11 20:26:08 +0100

    fix: increased waiting time when receiving SMIA registration confirmation for SMIA platform performance evaluation

--- src/smia/states/state_booting.py
+++ src/smia/states/state_booting.py
@@ -115,7 +115,7 @@ class StateBooting(State):
                      "instance in the SMIA KB.".format(smia_ism_jid))
         await self.send(register_acl_msg)
         _logger.info("Waiting for the confirmation of the registry in the SMIA KB...")
-        msg = await self.receive(timeout=30)  # Timeout set to 10 seconds
+        msg = await self.receive(timeout=60)  # Timeout set to 10 seconds
         if msg:
             valid_msg_template = GeneralUtils.create_acl_template(
                 FIPAACLInfo.FIPA_ACL_PERFORMATIVE_INFORM, ACLSMIAOntologyInfo.ACL_ONTOLOGY_AAS_INFRASTRUCTURE_SERVICE)
@@ -156,7 +156,7 @@ class StateBooting(State):
                      "KB.".format(smia_ism_jid))
         await self.send(register_acl_msg)
         _logger.info("Waiting for the confirmation of the registry in the SMIA KB...")
-        msg = await self.receive(timeout=30)  # TODO DELETE: CHANGED FOR REGISTRATION PERFORMANCE TEST
+        msg = await self.receive(timeout=60)  # TODO DELETE: CHANGED FOR REGISTRATION PERFORMANCE TEST
         # msg = await self.receive(timeout=5)  # Timeout set to 5 seconds
         _logger.assetinfo("HA PASADO DEL RECEIVE")  # TODO BORRAR
         if msg:
