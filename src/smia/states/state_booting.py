69e2b5d07865cb440cf4e391a377bde35c8c7624
ekhurtado <e16hurtado@gmail.com>
2026-02-12 10:23:44 +0100

    fix: increased waiting time when receiving SMIA registration confirmation for SMIA platform performance evaluation

--- src/smia/states/state_booting.py
+++ src/smia/states/state_booting.py
@@ -129,7 +129,8 @@ class StateBooting(State):
                      "instance in the SMIA KB.".format(smia_ism_jid))
         await self.send(register_acl_msg)
         _logger.info("Waiting for the confirmation of the registry in the SMIA KB...")
-        msg = await self.receive(timeout=60)  # Timeout set to 10 seconds
+        msg = await self.receive(timeout=150)  # TODO DELETE: CHANGED FOR REGISTRATION PERFORMANCE TEST
+        # msg = await self.receive(timeout=10)  # Timeout set to 10 seconds
         if msg:
             valid_msg_template = GeneralUtils.create_acl_template(
                 FIPAACLInfo.FIPA_ACL_PERFORMATIVE_INFORM, ACLSMIAOntologyInfo.ACL_ONTOLOGY_AAS_INFRASTRUCTURE_SERVICE)
@@ -170,8 +171,8 @@ class StateBooting(State):
                      "KB.".format(smia_ism_jid))
         await self.send(register_acl_msg)
         _logger.info("Waiting for the confirmation of the registry in the SMIA KB...")
-        msg = await self.receive(timeout=60)  # TODO DELETE: CHANGED FOR REGISTRATION PERFORMANCE TEST
-        # msg = await self.receive(timeout=5)  # Timeout set to 5 seconds
+        msg = await self.receive(timeout=150)  # TODO DELETE: CHANGED FOR REGISTRATION PERFORMANCE TEST
+        # msg = await self.receive(timeout=10)  # Timeout set to 10 seconds
         _logger.assetinfo("HA PASADO DEL RECEIVE")  # TODO BORRAR
         if msg:
             _logger.assetinfo("MENSAJE RECEIVE: {}".format(msg))  # TODO BORRAR
