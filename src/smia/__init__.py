90f5a2a2c03bca47a172fa147868af50c0c6237a
ekhurtado <e16hurtado@gmail.com>
2026-03-03 19:10:12 +0100

    feat: bump version in 'pyproject.toml' for SMIA release

--- src/smia/__init__.py
+++ src/smia/__init__.py
@@ -4,7 +4,7 @@ This is the main package of SMIA, that includes all source code and all subpacka
 
 __author__ = """Ekaitz Hurtado"""
 __email__ = "ekaitz.hurtado@ehu.eus"
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 __all__ = ['launchers', 'agents', 'utilities', 'assetconnection']
 
